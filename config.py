357b81cd717857916f311beee1a9e4b4f1c74274
Mᴏᴏɴʟɪɢʜᴛ <87492539+Lost-In-Dark@users.noreply.github.com>
2021-10-16 19:47:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,21 +7,11 @@ from pytgcalls import PyTgCalls
 if os.path.exists(".env"):
     load_dotenv(".env")
     
-# For Heroku
-HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")
-HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
-
 # Necessary Vars
 API_ID = int(os.getenv("API_ID", "6"))
 API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 SESSION = os.getenv("SESSION")
 HNDLR = os.getenv("HNDLR", "!")
-
-# Check
-if os.getenv("DYNO"):
-    ON_HEROKU = True
-else:
-    ON_HEROKU = False
 
 
 contact_filter = filters.create(
@@ -29,6 +19,5 @@ contact_filter = filters.create(
     (message.from_user and message.from_user.is_contact) or message.outgoing
 )
 
-UPSTREAM_REPO = "https://github.com/Lost-In-Dark/TG-MusicPlayer"
 bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="VCBot"))
 call_py = PyTgCalls(bot)
