db33702da136c44477ff107040ac6ee91ca27b27
Mᴏᴏɴʟɪɢʜᴛ <87492539+Lost-In-Dark@users.noreply.github.com>
2021-11-10 05:39:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,18 +12,12 @@ API_ID = int(os.getenv("API_ID", "6"))
 API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 SESSION = os.getenv("SESSION")
 HNDLR = os.getenv("HNDLR", "!")
-GROUP_MODE = os.getenv("GROUP_MODE", "True")
+
 
 contact_filter = filters.create(
     lambda _, __, message:
     (message.from_user and message.from_user.is_contact) or message.outgoing
 )
 
-if GROUP_MODE == "True" or "true":
-    grp = True
-else:
-    grp = False
-
-GRPPLAY = grp
 bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="VCBot"))
 call_py = PyTgCalls(bot)
